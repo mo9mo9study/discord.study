aa6f60007c15fa1a358f086122f978f1b3b4162a
su-pleiades <su.impreza.itengineer@gmail.com>
2020-01-26 13:35:13 +0900

    modify observer.sh

--- enter_exit/observer.sh
+++ enter_exit/observer.sh
@@ -5,7 +5,7 @@ strStatus=$(echo $?)
 today=$(date "+%Y/%m/%d/%H:%M:%S")
 if [ ${strStatus} -eq 1 ]; then
     source /home/ec2-user/discord/enter_exit/.env
-    /usr/bin/python3 /home/ec2-user/discord/enter_exit/enter_exit_bot.py & >> /var/log/discord/enterBot/success.log 2>> /var/log/discord/enterBot/error.log
+    /usr/bin/python3 /home/ec2-user/discord/enter_exit/enter_exit_bot.py 1>> /var/log/discord/enterBot/success.log 2>> /var/log/discord/enterBot/error.log
     echo "${today} - bot not found. restart." >> /var/log/discord/enterBot/observer.log
 elif [ ${strStatus} -ne 0 -o ${strStatus} -ne 1 ]; then
     echo "${today} - OK!" >> /var/log/discord/enterBot/observer.log
