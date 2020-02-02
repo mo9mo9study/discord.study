b77d92ca56bcf6037f91627cd449084626a8d0c7
su-pleiades <su.impreza.itengineer@gmail.com>
2020-02-02 11:26:40 +0900

    modify test.py

---
+++ enter_exit/test.py
@@ -0,0 +1,12 @@
+import os
+
+#print("abs",os.path.dirname(os.path.abspath(__file__)))
+#fistfile=os.path.dirname(os.path.abspath(__file__))
+#str = "timelog"
+#strf = fistfile + "/" + str
+#print(strf)
+
+print("dirname" + os.path.dirname())
+print("abs" + os.path.abspath())
+##
+
