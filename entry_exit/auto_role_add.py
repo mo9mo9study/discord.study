fd92dd7b2bf8c1db0fe71c0d8302d64d3350ef5c
C <8937866+floof-ball@users.noreply.github.com>
2020-03-29 19:09:55 +0900

    fix auto role add

--- entry_exit/auto_role_add.py
+++ entry_exit/auto_role_add.py
@@ -15,7 +15,7 @@ async def on_voice_state_update(member, before, after):
     if member.name != 'ブレーメン音楽隊':
         if member.guild.id == SERVER and (before.channel != after.channel):
             guild = client.get_guild('') # FIXME: guild_id
-            roles = guild.get_role('') # FIXME: role_id（ `ラウンジ用チャット` の表示/非表示を切り替える）
+            roles = guild.get_role('') # FIXME: role_id（ `ラウンジ用チャット` の表示/非表示のための権限）
             if before.channel is None:
                 if after.channel.name == 'ラウンジ（会話有）':
                     await member.add_roles(roles)
