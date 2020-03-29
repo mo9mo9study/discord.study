c0dfe971f195a45a19e7a766e71d8b364efdda10
C <8937866+floof-ball@users.noreply.github.com>
2020-03-29 19:09:33 +0900

    fix auto role add

--- entry_exit/auto_role_add.py
+++ entry_exit/auto_role_add.py
@@ -14,11 +14,14 @@ SERVER = setting.dServer
 async def on_voice_state_update(member, before, after):
     if member.name != 'ブレーメン音楽隊':
         if member.guild.id == SERVER and (before.channel != after.channel):
+            guild = client.get_guild('') # FIXME: guild_id
+            roles = guild.get_role('') # FIXME: role_id（ `ラウンジ用チャット` の表示/非表示を切り替える）
             if before.channel is None:
-                if after.channel.name == 'リモートワーク用':
-                    discord.unils.get(message
+                if after.channel.name == 'ラウンジ（会話有）':
+                    await member.add_roles(roles)
             elif after.channel is None:
-                if before.channel.name == 'リモートワーク用':
+                if before.channel.name == 'ラウンジ（会話有）':
+                    await member.remove_roles(roles)
             
 
 client.run(setting.dToken)
