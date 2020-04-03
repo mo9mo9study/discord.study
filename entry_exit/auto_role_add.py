e9d0ff73cddb5db0e823d1cf9eda36a0e0e23f70
C <8937866+floof-ball@users.noreply.github.com>
2020-04-03 20:54:55 +0900

    作業部屋用チャットの表示非表示機能

--- entry_exit/auto_role_add.py
+++ entry_exit/auto_role_add.py
@@ -15,13 +15,12 @@ async def on_voice_state_update(member, before, after):
     if member.name != 'ブレーメン音楽隊':
         if member.guild.id == SERVER_ID and (before.channel != after.channel):
             guild = client.get_guild(SERVER_ID)
-            lounge_channel_role = guild.get_role('') # FIXME: role_id（ `ラウンジ用チャット` を表示/非表示させるための権限）
+            lounge_channel_role = guild.get_role(684959750236143639)
             if before.channel is None:
-                if after.channel.name == 'ラウンジ（会話有）':
+                if after.channel.name == '作業部屋用チャット':
                     await member.add_roles(lounge_channel_role)
             elif after.channel is None:
-                if before.channel.name == 'ラウンジ（会話有）':
+                if before.channel.name == '作業部屋用チャット':
                     await member.remove_roles(lounge_channel_role)
-            
 
 client.run(setting.dToken)
