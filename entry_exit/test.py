0a5d10db75294900a57d8e11442d63a1f0a9998e
su-pleiades <su.impreza.itengineer@gmail.com>
2020-04-14 11:56:57 +0900

    on_voice_state_updateのmenbar値を出力するため

--- entry_exit/test.py
+++ entry_exit/test.py
@@ -1,12 +1,20 @@
 import os
+import setting
+import discord1
+
+TOKEN = cToken
 
 #print("abs",os.path.dirname(os.path.abspath(__file__)))
 #fistfile=os.path.dirname(os.path.abspath(__file__))
 #str = "timelog"
 #strf = fistfile + "/" + str
 #print(strf)
+#print("dirname" + os.path.dirname())
+#print("abs" + os.path.abspath())
 
-print("dirname" + os.path.dirname())
-print("abs" + os.path.abspath())
-##
+client = discord.Client()
+@client.event
+async def on_voice_state_update(member, before, after):
+    print(member)
 
+client.run(TOKEN)
