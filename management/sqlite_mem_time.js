8db1c6164640dfd17919d652a73effc76b4452ce
su-pleiades <su.impreza.itengineer@gmail.com>
2020-05-05 16:16:03 +0900

    [#15] 全ユーザーの情報を抽出し、insertする処理完了
    繰り返し処理の中でdb.closeをコメントアウトにすることで正常に動作した
    次はupdateの処理を追加
    一旦基盤となるコードは書けたので、必要に応じて追加と修正していく

--- management/sqlite_mem_time.js
+++ management/sqlite_mem_time.js
@@ -22,59 +22,87 @@ const select_all = () => {
         console.log('row',row);
     });
 }
+// 見やすさ重視の理想形
+//   userid,
+//   username,
+//   guild_join_datetime,
+//   voicechannel_lastjoin_datetime,
+//   voicechannel_lastleave_datetime,
+//   message_lastsent_datetime
+//   ) VALUES (
+//   ${id},
+//   '${name}',
+//   '${guild_join_datetime}',
+//   '${voicechannel_lastjoin_datetime}',
+//   '${voicechannel_lastleave_datetime}',
+//   '{message_lastsent_datetime}',
+//`};
 // データゼロ想定の書き込み
 const insert_member = (userid, username, guild_join_datetime) => {
-    db.each("SELECT * FROM mem_time WHERE userid = ? and username = ?",[userid,username],(error, row) => {
-        if(error){
-            console.log('Error!:', error);
-            return;
-        }
-    }, (error, count) => {  //←complete処理追加
-        //SQL実行直後に上のコールバック処理が走る前にここが処理されるで。
-        if (error){
-            //エラー処理
-            console.log('Error!:', error);
-        }else{
-            if (count == 0){
-                //ここでSQLの実行結果が0件の処理や
-                db.run("INSERT INTO mem_time(userid, username, guild_join_datetime) VALUES(?,?,?);",[userid, username, guild_join_datetime])
-                console.log(`${userid}(${username})を新規追加(INSERT)しました`);
-            }else{
-                console.log(`既にDBに${userid}(${username})が存在します`);
+    db.serialize(() => {
+        db.each("SELECT * FROM mem_time WHERE userid = ? and username = ?",[userid,username],(error, row) => {
+            if(error){
+                console.log('Error!:', error);
+                return;
             }
-        }
+        }, (error, count) => {  //←complete処理追加
+            //SQL実行直後に上のコールバック処理が走る前にここが処理されるで。
+            if (error){
+                //エラー処理
+                console.log('Error!:', error);
+            }else{
+                if (count == 0){
+                    //ここでSQLの実行結果が0件の処理や
+                    let sql = db.prepare(
+                        "INSERT INTO mem_time(userid, username,guild_join_datetime) VALUES(?,?,?)",
+                        userid, username, guild_join_datetime,
+                        );
+                    sql.run();
+                    console.log(`${userid}(${username})を新規追加(INSERT)しました`);
+                }else{
+                    console.log(`既にDBに${userid}(${username})が存在します`);
+                }
+            }
+        });
+//ここをコメントすることで「Segmentation fault」エラーがなくなって書き込めた
+//        db.close(
+//            function(err) {
+//                if (err) {
+//                    console.error(err.message);
+//                }
+//                else {
+//                    console.log("db Close.");
+//                }
+//            }
+//        );
     });
 } 
-
-//insert_member(603567991132782600,'SuPleiades');
-
 // 送信されたメッセージをトリガーに処理開始
 client.on('message', message => {
     let channel = message.channel;
     let author = message.author.username;
-//    if(message.content === '!all_insert_jointime') {
-    console.log('---command---');
-    const members = message.guild.members.cache.keyArray();
-    members.forEach(function(value) {
-        const name = message.guild.members.cache.get(value);
-        console.log('------------------------')
-        console.log('name.user.id: ',name.id);
-        console.log('name.user.username: ',name.user.username);
-        console.log('name.joinedTimestamp: ',name.joinedTimestamp);
-
-//        db.serialize(() => {
-//            insert_member(name.id, name.user.username, name.joinedTimestamp);
-//            db.finalize();
-//        });
-    db.close();
-//    var date = new Date(name.joinedTimestamp)
+    if(message.content === '¥all_insert_jointime') {
+        console.log('---command---');
+        const members = message.guild.members.cache.keyArray();
+        members.forEach(function(value) {
+            const name = message.guild.members.cache.get(value);
+            insert_member(
+                name.user.id,
+                name.user.username,
+                name.joinedTimestamp
+            )
+            console.log('------------------------')
+            console.log('name.user.id: ',name.user.id);
+            console.log('name.user.username: ',name.user.username);
+            console.log('name.joinedTimestamp: ',name.joinedTimestamp);
 //    // メッセージへリアクション
 //    message.reply(reply_text)
 //        .then(message => console.log(`Sent message: ${reply_text}`))
 //        .catch(console.error);
 //    message.delete({ timeout: 1000 })
 //    return;
-    });
+        });
+    }
 });
 
 client.login(TOKEN);
