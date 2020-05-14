c5a002be15c5845e4b7ece0888858ab01f8a6df0
su-pleiades <su.impreza.itengineer@gmail.com>
2020-05-14 17:51:46 +0900

    [ Close #23 ] ボイスチャンネル 参加時にMusicBot操作用のチャンネルが表示される様にしました
    ボイスチャンネル 参加時：チャンネルに紐づけられた閲覧用の役職が付与され、表示される
    ボイスチャンネル 退室時：チャンネルに紐づけられた閲覧用の役職が剥奪され、表示されなくなる
    全チャンネルを対象にしているが、もくもく勉強机はBOT呼べないので表示される必要あるかどうか検討

---
+++ management/voiceChannelJoinLeave_roleModify.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+const Discord = require('discord.js');
+const env = process.env;
+const TOKEN = env.MANAGER_BOT_TOKEN
+const client = new Discord.Client()
+const SERVER = env.DISCORD_SEVER_ID
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('voiceStateUpdate', (before, after) => {
+    let userid,
+        username,
+        guild_join_datetime,
+        voicechannel_lastjoin_datetime,
+        voicechannel_lastleave_datetime,
+        message_lastsent_datetime;
+    userid = after.id;
+    const member = after.guild.members.cache.get(userid)
+    const roleid = '710333297598922752';
+    console.log('before: ',before.channelID,'(after) ',after.channelID)
+    if( after.guild.id === SERVER && (before.channelID != after.channelID) ) {
+        console.log('viceStateUpdate')
+        if( before.channelID == null ){
+            console.log(`${member.user.username}(${userid})がボイスチャンネルに参加しました`);
+            member.roles.add(roleid);
+        }
+        if( after.channelID == null ){
+            console.log(`${member.user.username}(${userid})がボイスチャンネル から退室しました`);
+            // ロール付与されているかの確認をいれてエラーをなくす
+            member.roles.remove(roleid);
+        }
+    }
+});
+
+
+client.login(TOKEN);
