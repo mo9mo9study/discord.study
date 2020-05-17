a2d0fb3f21fe68e5960150cb6871170d157d7e0d
su-pleiades <su.impreza.itengineer@gmail.com>
2020-05-17 11:22:18 +0900

    記録対象除外方法をコード内のリストから、Discordの.bot(bloom)から判定するように変更
    {message.bot}がfalseなら記録メンバーとして、
    trueならBOTなので記録対象外とする処理に変更

--- entry_exit/enter_exit_bot.py
+++ entry_exit/enter_exit_bot.py
@@ -9,18 +9,13 @@ pretime_dict = {}
 botScriptPath = os.path.dirname(os.path.abspath(__file__))
 logsDirectory = 'timelog'
 NotRecordChannels = '時間記録無'
-NotRecordMembers = ["ブレーメン音楽隊","ブレーメン音楽隊２"]
 
-
-
-##
 logsPath = botScriptPath +"/"+ logsDirectory +"/"
 print(logsPath)
-##
 
 
 def writeLog(time,name,m,mdta=''):
-    print("<ログの書き込み>")
+    print(f'{name} : <ログの書き込み>')
     if not os.path.isdir(logsPath):
         os.mkdir(logsPath)
     filePath = f'{logsPath}{name}'
@@ -32,7 +27,7 @@ def writeLog(time,name,m,mdta=''):
             f.write(str(time) + ',' + name + ',' + m + ',' + mdta + '\n')
 
 def splitTime(name):
-    print("<ログの読み込み>")
+    print(f'{name} : <ログの読み込み>')
     filePath = f'{logsPath}{name}'
     with open(filePath) as f:
         arrAlllog = f.readlines()
@@ -46,26 +41,25 @@ def splitTime(name):
 @client.event
 async def on_voice_state_update(member, before, after):
     print(member.name + " : ステータスが変更されました")
-    if member.name in NotRecordMembers:
-        print(member.name + ": NotRecordMembersリストに記載されているので記録しません")
+    if member.bot:
+        print(f'{member.name} : BOTは記録しません')
         return
     if member.guild.id == setting.dServer and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
         alert_channel = client.get_channel(setting.dChannel)
-
         if before.channel is None:
-            print("入室時の処理")
+            print(f'{member.name} : 入室時の処理')
             pretime_dict['beforetime'] = datetime.now()
             msg = f'{now:%m/%d %H:%M} 　 {member.name}   joined the  {after.channel.name}'
             writeLog(datetime.now(),member.name,msg)
             if  NotRecordChannels in after.channel.name:
-                print("NotRecordChannelsリストに記載されているので記録しません")
+                print(f'{member.name} : NotRecordChannelsリストに記載されているので記録しません')
                 return
             await alert_channel.send(msg)
         elif after.channel is None:
-            print("退室時の処理")
+            print(f'{member.name} : 退室時の処理')
             if NotRecordChannels in before.channel.name:
-                print("NotRecordChannelsリストに記載されているので記録しません")
+                print(f'{member.name} : NotRecordChannelsリストに記載されているので記録しません')
                 return
             msg = f'[{now:%m/%d %H:%M} ]  {member.name}  joined  {before.channel.name} '
             dtBefortime = splitTime(member.name)
@@ -73,16 +67,15 @@ async def on_voice_state_update(member, before, after):
                 duration_time = dtBefortime - datetime.now()
                 duration_time_adjust = int(duration_time.total_seconds()) * -1
                 if duration_time_adjust >= 60:
-                    print("60sec以上")
+                    print(f'{member.name} : 60sec以上')
                     minute_duration_time_adjust = int(duration_time_adjust) // 60
                     msg = "-->[" + member.name + "]   Study time： " + str(minute_duration_time_adjust) + "/分"
                     writeLog(datetime.now(),member.name,msg,str(minute_duration_time_adjust))
                     if duration_time_adjust >= 300:
-                        print("退室ログをDiscordに出力")
+                        print(f'{member.name} : 退室ログをDiscordに出力')
                         await alert_channel.send(msg)
             except KeyError:
-                print("except")
+                print(f'{member.name} : except')
                 pass
             
-
 client.run(setting.dToken)
