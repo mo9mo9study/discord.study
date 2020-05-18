339bcc959a0252ca0cde1a9455202c698434555c
su-pleiades <su.impreza.itengineer@gmail.com>
2020-05-18 21:17:01 +0900

    [#24] チャンネルが作成されない不具合を修正
    == を includes に変更しました

--- management/autoCreateTimes.js
+++ management/autoCreateTimes.js
@@ -5,6 +5,7 @@ const env = process.env;
 const TOKEN = env.MANAGER_BOT_TOKEN
 const client = new Discord.Client()
 const CHANNELID = '615185771565023244' // チャンネル「(初回)自己紹介」
+const categoryid = '673004651871993866'
 
 const sendMessage = 'このチャンネルはあなたの分報(個人)チャンネルです。\n使い方は「参考資料」又は「他メンバーの分報チャンネル」をご覧ください。\n上手に活用してみてくださいね\n 　参考資料：http://c16e.com/1511101558/';
 
@@ -26,16 +27,22 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if( message.channel.id != CHANNELID ) return
-    const channelName = ('times_' + message.author.username).toLowerCase()
-    if( message.content == '呼び名') {
-        if( allChannelNameList(message.channel.guild.channels.cache).includes( channelName )) {
+    const channels = allChannelNameList(message.channel.guild.channels.cache);
+    const channelName = ('times_' + message.author.username).toLowerCase();
+    console.log(`timesチャンネルは${channels.length}個存在します`);
+    if( message.content.includes( '呼び名' )) {
+        if( channels.length > 50 ) {
+            console.log( `timesチャンネルが${channels.length}を超えました`);
+        }
+        if( channels.includes( channelName )) {
             console.log('既にチャンネル名が存在する');
         } else {
             message.channel.guild.channels.create( channelName , {
                 type: 'text',
-                parent: '711028917598945310',
+                parent: categoryid,
                 topic: message.author.id
             });
+            console.log(`${channelName}を作成しました`);
         }
     }
 });
