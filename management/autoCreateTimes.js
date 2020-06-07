697cc59388b393d449e5b7c25e9106b0533c6e2c
su-pleiades <su.impreza.itengineer@gmail.com>
2020-06-07 15:36:51 +0900

    自動通報追加＆変数の外部化＆作成されるカテゴリー先を変更
    timesチャンネルから５０個を超えたらオーナーの分報チャンネルにその旨を連絡する機能追加
    変数をenvに移動しました
    作成されるカテゴリー先をACTIVE_TIMESに変更しました（従来は分報チャンネルカテゴリーに作成されていた）

--- management/autoCreateTimes.js
+++ management/autoCreateTimes.js
@@ -4,8 +4,8 @@ const Discord = require('discord.js');
 const env = process.env;
 const TOKEN = env.MANAGER_BOT_TOKEN
 const client = new Discord.Client()
-const CHANNELID = '615185771565023244' // チャンネル「(初回)自己紹介」
-const categoryid = '673004651871993866'
+const CHANNELID = env.SELF_INTRODUCTION_CHANEL_ID 
+const categoryid = env.ACTIVE_TIMES_CATEGORY_ID
 
 const sendMessage = 'このチャンネルはあなたの分報(個人)チャンネルです。\n使い方は「参考資料」又は「他メンバーの分報チャンネル」をご覧ください。\n上手に活用してみてくださいね\n 　参考資料：http://c16e.com/1511101558/';
 
@@ -31,12 +31,19 @@ client.on('message', message => {
     const channelName = ('times_' + message.author.username).toLowerCase();
     console.log(`timesチャンネルは${channels.length}個存在します`);
     if( message.content.includes( '呼び名' )) {
-        if( channels.length > 50 ) {
-            console.log( `timesチャンネルが${channels.length}を超えました`);
-        }
         if( channels.includes( channelName )) {
-            console.log('既にチャンネル名が存在する');
+            console.log('既にチャンネル名が存在します');
         } else {
+            if( channels.length > 50 ) {
+                let alertMessage = `timesチャンネルが50個(現在：${channels.length}個)を超えました`;
+		// 管理者の文法チャンネルにメンション付きで連絡する機能
+                const member = message.channel.guild.members.cache.get(message.channel.guild.ownerID);
+                // おいおいは、チャンネルのトピックにownerIDが含まれているチャンネルを選択するやり方にする
+		const channel = message.channel.guild.channels.cache.get('673006702924136448');
+                channel.send( alertMessage, {reply: member } )
+                    .then(console.log(`${channel.name}に「分報チャンネル」の総数が50個を超えた旨を連絡しました`))
+                    .catch(console.error);
+            }
             message.channel.guild.channels.create( channelName , {
                 type: 'text',
                 parent: categoryid,
