f0b4cd56ea1c5bfb517e32c5fd42b323a315350d
nntsugu <nntsugu@gmail.com>
2020-08-01 00:10:15 +0900

    update observe.sh

--- entry_exit/observer.sh
+++ entry_exit/observer.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-<<<<<<< HEAD
-bash $(pwd)/entry_exit/enter_exit_bot.sh
-#bash post_week_result.sh
-=======
 bash $(pwd)/entry_exit/enter_exit_bot.sh &
 bash $(pwd)/entry_exit/post_week_result.sh &
->>>>>>> update observe.sh
 while :; do
     sleep 1
 done
