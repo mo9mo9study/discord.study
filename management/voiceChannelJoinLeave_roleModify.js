d56571f2f5ae633d57db7e6dcb2cc015ee00a5b5
yabutayukinari <yabutayukinari@gmail.com>
2020-10-03 01:03:21 +0900

    作業部屋、ラウンジ入室時にのみ各専用テキストチャットが表示できるように変更

--- management/voiceChannelJoinLeave_roleModify.js
+++ management/voiceChannelJoinLeave_roleModify.js
@@ -7,30 +7,58 @@ const client = new Discord.Client();
 const SERVER = env.DISCORD_SEVER_ID;
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("voiceStateUpdate", (before, after) => {
-  const userid = after.id;
-  const member = after.guild.members.cache.get(userid);
-  const roleid = "710333297598922752";
-  console.log("before: ", before.channelID, "(after) ", after.channelID);
-  if (after.guild.id === SERVER && before.channelID != after.channelID) {
-    console.log("viceStateUpdate");
-    if (before.channelID == null) {
-      console.log(
-        `${member.user.username}(${userid})がボイスチャンネルに参加しました`
-      );
-      member.roles.add(roleid);
+    const userid = after.id;
+    const member = after.guild.members.cache.get(userid);
+
+    // TODO: 2020/10/03 yabuta env等で定義できたらしたい。
+    // 「作業部屋用チャット」表示権限ID
+    const workRoomChatRoleId = "";
+    // 「ラウンジ用チャット」表示権限ID
+    const loungeChatRoleId = "";
+    // 「作業部屋」ID
+    const workRoomVoiceChatId = "";
+    // 「ラウンジ」ID
+    const loungeVoiceChatId = "";
+
+    console.log("before: ", before.channelID, "(after) ", after.channelID);
+    // 自分のギルドかつ、入室、退出の場合のみ
+    if (after.guild.id === SERVER && before.channelID !== after.channelID) {
+        console.log("viceStateUpdate");
+
+        // ロール削除
+        if(member.roles.cache.has(workRoomChatRoleId)){
+            member.roles.remove(workRoomChatRoleId);
+        }
+        if(member.roles.cache.has(loungeChatRoleId)){
+            member.roles.remove(loungeChatRoleId);
+        }
+
+        if (after.channelID == null) {
+            // 退出
+            console.log(
+                `${member.user.username}(${userid})がボイスチャンネル から退室しました`
+            );
+        } else if (before.channelID !== after.channelID) {
+            // 入室 or 部屋変更
+            console.log(
+                `${member.user.username}(${userid})がボイスチャンネルに参加しました`
+            );
+
+            // 作業部屋に入室
+            if (after.channelID === workRoomVoiceChatId) {
+                member.roles.add(workRoomChatRoleId);
+            }
+
+            // ラウンジに入室
+            if (after.channelID === loungeVoiceChatId) {
+                member.roles.add(loungeChatRoleId);
+            }
+        }
     }
-    if (after.channelID == null) {
-      console.log(
-        `${member.user.username}(${userid})がボイスチャンネル から退室しました`
-      );
-      // ロール付与されているかの確認をいれてエラーをなくす
-      member.roles.remove(roleid);
-    }
-  }
 });
 
 client.login(TOKEN);
